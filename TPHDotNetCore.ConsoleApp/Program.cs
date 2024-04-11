a8680741a91c3f7a8108bee5d6097e4492d1db32
paii29 <thetpainghein99@gmail.com>
2024-04-11 13:17:28 +0630

    Add project files.

---
+++ TPHDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+
+Console.WriteLine("Hello, World!");
+
+//nuget package(sqlclient) for database
+//F10 => 
+//F11 => into the used class
+
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+
+stringBuilder.DataSource = "DESKTOP-SOD2VVR"; //servername
+stringBuilder.InitialCatalog = "TPHDotNetCore"; //databasename
+
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa@123";
+
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+
+connection.Open();
+Console.WriteLine("Connection Open");
+
+string query = "select * from Tbl_Blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+
+connection.Close();
+Console.WriteLine("Connection Close");
+
+//dataset => datatable
+//datatable => datarow
+//datarow => datacolumn
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog ID =>" + dr["BlogID"]);
+    Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+    Console.WriteLine("--------------------------");
+}
+
+
+//Ado.net Read
+
+Console.ReadKey();
