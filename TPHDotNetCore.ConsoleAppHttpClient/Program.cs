c6faa41eb1d587b312aa6759d023425a1e08f0f6
paii29 <thetpainghein99@gmail.com>
2024-05-17 01:12:17 +0630

    [Complete] Burma-Project PickAPile

---
+++ TPHDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+string jsonStr = await File.ReadAllTextAsync("data.json");
+var model =  JsonConvert.DeserializeObject<MainDto>(jsonStr);
+//Console.WriteLine(jsonStr);
+
+foreach (var question in model.questions)
+{
+    Console.WriteLine(question.questionName);
+}
+
+// Json to c# ??? package
+// C# to Json
+
+Console.ReadLine();
+
+static string MmToEngNum(string num)
+{
+   num = num.Replace("၁", "1");
+   num = num.Replace("၂", "2");
+   num = num.Replace("၃", "3");
+   num = num.Replace("၄", "4");
+   num = num.Replace("၅", "5");
+   num = num.Replace("၆", "6");
+   num = num.Replace("၇", "7");
+   num = num.Replace("၈", "8");
+   num = num.Replace("၉", "9");
+   num = num.Replace("၁၀","10");
+
+   return num;
+}
+
+public class MainDto
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
---
+++ TPHDotNetCore.RestApiWithNLayer/Features/PickAPile/PickAPileController.cs
@@ -0,0 +1,91 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace TPHDotNetCore.RestApiWithNLayer.Features.PickAPile
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PickAPileController : ControllerBase
+    {
+
+        private PickAPile _data;
+
+        private async Task<PickAPile> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("PickAPile.json");
+            var model = JsonConvert.DeserializeObject<PickAPile>(jsonStr);
+            return model;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AllQuestions()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Questions);
+        }
+
+        [HttpGet("{questionId}")]
+        public async Task<IActionResult> Question(int questionId)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.Questions.FirstOrDefault(x => x.QuestionId == questionId));
+        }
+
+        [HttpGet("{questionNo}/{pileNo}")]
+        public async Task<IActionResult> Answer(int questionNo, int pileNo)
+        {
+            var model = await GetDataAsync();
+
+            string pileName;
+            if (pileNo == 1)
+            {
+                pileName = "Pile-1 ";
+            }
+            else if (pileNo == 2)
+            {
+                pileName = "Pile-2";
+            }
+            else if (pileNo == 3)
+            {
+                pileName = "Pile-3";
+            }
+            else if (pileNo == 4)
+            {
+                pileName = "Pile-4";
+            }
+            else pileName = "No pile";
+
+
+            return Ok(model.Answers.FirstOrDefault(x => x.QuestionId == questionNo && x.AnswerName == pileName));
+
+        }
+
+    }
+
+
+    public class PickAPile
+    {
+        public Question[] Questions { get; set; }
+        public Answer[] Answers { get; set; }
+    }
+
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string QuestionName { get; set; }
+        public string QuestionDesp { get; set; }
+    }
+
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        public string AnswerImageUrl { get; set; }
+        public string AnswerName { get; set; }
+        public string AnswerDesp { get; set; }
+        public int QuestionId { get; set; }
+    }
+
+
+}
