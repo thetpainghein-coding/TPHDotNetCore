6ea109400d66dbbf38c78d03e87c5e9e19482047
paii29 <thetpainghein99@gmail.com>
2024-05-28 19:15:28 +0630

    [Complete] WinForm Read using DataGridView

---
+++ TPHDotNetCore.WinFormsApp/FrmBlogList.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace TPHDotNetCore.WinFormsApp
+{
+    partial class FrmBlogList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvData = new DataGridView();
+            colId = new DataGridViewTextBoxColumn();
+            colEdit = new DataGridViewButtonColumn();
+            colRemove = new DataGridViewButtonColumn();
+            colTitle = new DataGridViewTextBoxColumn();
+            colAuthor = new DataGridViewTextBoxColumn();
+            colContent = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvData).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvData
+            // 
+            dgvData.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvData.Columns.AddRange(new DataGridViewColumn[] { colId, colEdit, colRemove, colTitle, colAuthor, colContent });
+            dgvData.Dock = DockStyle.Fill;
+            dgvData.EditMode = DataGridViewEditMode.EditOnEnter;
+            dgvData.Location = new Point(0, 0);
+            dgvData.Name = "dgvData";
+            dgvData.RowHeadersWidth = 62;
+            dgvData.RowTemplate.Height = 33;
+            dgvData.Size = new Size(800, 450);
+            dgvData.TabIndex = 0;
+            // 
+            // colId
+            // 
+            colId.DataPropertyName = "BlogId";
+            colId.HeaderText = "ID";
+            colId.MinimumWidth = 8;
+            colId.Name = "colId";
+            colId.Visible = false;
+            // 
+            // colEdit
+            // 
+            colEdit.HeaderText = "";
+            colEdit.MinimumWidth = 8;
+            colEdit.Name = "colEdit";
+            colEdit.Text = "Edit";
+            colEdit.UseColumnTextForButtonValue = true;
+            // 
+            // colRemove
+            // 
+            colRemove.HeaderText = "";
+            colRemove.MinimumWidth = 8;
+            colRemove.Name = "colRemove";
+            colRemove.Text = "Delete";
+            colRemove.UseColumnTextForButtonValue = true;
+            // 
+            // colTitle
+            // 
+            colTitle.DataPropertyName = "BlogTitle";
+            colTitle.HeaderText = "Title";
+            colTitle.MinimumWidth = 8;
+            colTitle.Name = "colTitle";
+            colTitle.ReadOnly = true;
+            // 
+            // colAuthor
+            // 
+            colAuthor.DataPropertyName = "BlogAuthor";
+            colAuthor.HeaderText = "Author";
+            colAuthor.MinimumWidth = 8;
+            colAuthor.Name = "colAuthor";
+            colAuthor.ReadOnly = true;
+            // 
+            // colContent
+            // 
+            colContent.DataPropertyName = "BlogContent";
+            colContent.HeaderText = "Content";
+            colContent.MinimumWidth = 8;
+            colContent.Name = "colContent";
+            colContent.ReadOnly = true;
+            // 
+            // FrmBlogList
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvData);
+            Name = "FrmBlogList";
+            Text = "FrmBlogList";
+            Load += FrmBlogList_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvData).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvData;
+        private DataGridViewTextBoxColumn colId;
+        private DataGridViewButtonColumn colEdit;
+        private DataGridViewButtonColumn colRemove;
+        private DataGridViewTextBoxColumn colTitle;
+        private DataGridViewTextBoxColumn colAuthor;
+        private DataGridViewTextBoxColumn colContent;
+    }
+}
---
+++ TPHDotNetCore.WinFormsApp/FrmBlogList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TPHDotNetCore.Shared;
+using TPHDotNetCore.WinFormsApp.Models;
+
+namespace TPHDotNetCore.WinFormsApp
+{
+    public partial class FrmBlogList : Form
+    {
+        private readonly DapperService _dapperService;
+        public FrmBlogList()
+        {
+            InitializeComponent();
+            dgvData.AutoGenerateColumns = false;
+            _dapperService = new DapperService(ConnectionStrings.sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        private void FrmBlogList_Load(object sender, EventArgs e)
+        {
+            List<BlogModel> lst = _dapperService.Query<BlogModel>(@"SELECT [BlogId]
+                                                                      ,[BlogTitle]
+                                                                      ,[BlogAuthor]
+                                                                      ,[BlogContent]
+                                                                  FROM [dbo].[Tbl_Blog]");
+            dgvData.DataSource = lst;
+        }
+    }
+}
