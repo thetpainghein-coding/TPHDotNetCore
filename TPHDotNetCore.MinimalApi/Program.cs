0bea3cbe045f55572ba94cb686be589e66555a8e
paii29 <thetpainghein99@gmail.com>
2024-06-19 19:56:38 +0630

    [Complete} MVC Read

--- TPHDotNetCore.MinimalApi/Program.cs
+++ TPHDotNetCore.MinimalApi/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
 }, ServiceLifetime.Transient, ServiceLifetime.Transient);
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ TPHDotNetCore.MvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TPHDotNetCore.MvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
