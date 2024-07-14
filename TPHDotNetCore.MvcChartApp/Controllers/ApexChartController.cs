bd9d08034317d71ec0646cb09c272a5bebf9ec19
paii29 <thetpainghein99@gmail.com>
2024-07-14 12:33:55 +0630

    [Complete] Mvc Apex Chart

---
+++ TPHDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
@@ -0,0 +1,35 @@
+﻿using TPHDotNetCore.MvcChartApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TPHDotNetCore.MvcChartApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult SimplePieChart()
+        {
+            PieChartModel model = new PieChartModel();
+            model.Lables = new List<string>() { "Team A", "Team B", "Team C", "Team D", "Team E" };
+            model.Series = new List<int> { 44, 55, 13, 43, 22 };
+
+            return View(model);
+        }
+
+        public IActionResult CustomDataLabelsBarChart()
+        {
+            CustomDataLabelsBarChartModel model = new CustomDataLabelsBarChartModel();
+            model.Data = new List<int> { 400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380 };
+            model.Colors = new List<string>
+            {
+                "#33b2df", "#546E7A", "#d4526e", "#13d8aa", "#A5978B", "#2b908f", "#f9a3a4", "#90ee7e",
+            "#f48024", "#69d2e7"
+            };
+            model.Categories = new List<string>
+            {
+                "South Korea", "Canada", "United Kingdom", "Netherlands", "Italy", "France", "Japan",
+            "United States", "China", "India"
+            };
+            return View(model);
+        }
+
+    }
+}
---
+++ TPHDotNetCore.MvcChartApp/Models/CustomDataLabelsBarChartModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TPHDotNetCore.MvcChartApp.Models
+{
+    public class CustomDataLabelsBarChartModel
+{
+    public List<int> Data { get; set; }
+    public List<string> Colors { get; set; }
+
+    public List<string> Categories { get; set; }
+}
+}
