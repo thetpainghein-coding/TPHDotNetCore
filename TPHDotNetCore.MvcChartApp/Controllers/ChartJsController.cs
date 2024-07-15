399039ccbc2cf85f18d38d474afdced123b86e1d
paii29 <thetpainghein99@gmail.com>
2024-07-15 11:55:51 +0630

    [Complete] Chartjs

---
+++ TPHDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TPHDotNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ExampleChart()
+        {
+            return View();
+        }
+
+        public IActionResult BorderRadiusBarChart()
+        {
+            return View();
+        }
+
+        public IActionResult InterpolationModesChart()
+        {
+            return View();
+        }
+
+
+    }
+}
