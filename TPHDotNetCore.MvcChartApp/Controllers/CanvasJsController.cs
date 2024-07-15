7072bfeaebc46654fc3e371abd5e44f912785259
paii29 <thetpainghein99@gmail.com>
2024-07-15 12:16:31 +0630

    [Complete] Canvas JS

---
+++ TPHDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TPHDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
