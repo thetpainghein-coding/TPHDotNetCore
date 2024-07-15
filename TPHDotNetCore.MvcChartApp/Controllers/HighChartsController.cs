99142e6fc7aa8b6d27806990733861d9ffc7c0a4
paii29 <thetpainghein99@gmail.com>
2024-07-15 12:11:37 +0630

    [Complete] HighCharts

---
+++ TPHDotNetCore.MvcChartApp/Controllers/HighChartsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TPHDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
